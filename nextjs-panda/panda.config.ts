73eb9609e149f9769320ea0dac9015ca3dda1f9a
IP <ilya.pavlovski@gmail.com>
2023-09-14 10:30:02 -0300

    Add class-hashing and keyframes to panda.

--- nextjs-panda/panda.config.ts
+++ nextjs-panda/panda.config.ts
@@ -13,9 +13,22 @@ export default defineConfig({
   // Useful for theme customization
   theme: {
     extend: {
+      keyframes: {
+        marquee: {
+          '0%': {
+            transform: 'translateX(0%)'
+          },
+          '100%': {
+            transform: 'translateX(-100%)'
+          }
+        }
+      },
       tokens: {}
     }
   },
+
+  // hash classnames for devtools readability
+  hash: { cssVar: false, className: true },
 
   // enable codegen components
   jsxFramework: 'react',
--- react-panda/panda.config.ts
+++ react-panda/panda.config.ts
@@ -13,6 +13,16 @@ export default defineConfig({
   // Useful for theme customization
   theme: {
     extend: {
+      keyframes: {
+        marquee: {
+          '0%': {
+            transform: 'translateX(0%)'
+          },
+          '100%': {
+            transform: 'translateX(-100%)'
+          }
+        }
+      },
       tokens: {
         fonts: {
           jakarta: { value: 'Plus Jakarta Sans, sans-serif' },
@@ -21,6 +31,9 @@ export default defineConfig({
       }
     }
   },
+
+  // hash classnames for devtools readability
+  hash: { cssVar: false, className: true },
 
   // enable codegen components
   jsxFramework: 'react',
