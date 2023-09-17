72d4ddbb92c04a072a94e7538a3afd4ee8f9e75a
IP <ilya.pavlovski@gmail.com>
2023-09-17 16:12:08 -0300

    Migrate react-panda from prettier to dprint.

--- react-panda/vite.config.ts
+++ react-panda/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
+import FullReload from 'vite-plugin-full-reload'
 
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react()], // FullReload(['src/app.tsx'])
 
     server: {
       port: parseInt(process.env.VITE_PORT!),
