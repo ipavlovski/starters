a4652705bd513fbf9c1961b4f5ad9f40c427dead
IP <ilya.pavlovski@gmail.com>
2024-01-09 00:26:12 -0400

    Update react-panda.

--- react-panda/src/app.tsx
+++ react-panda/src/app.tsx
@@ -3,7 +3,10 @@ import { css } from 'styled-system/css'
 export default function App() {
   const styles = css({
     background: 'slate.500',
-    color: 'slate.100'
+    color: 'slate.100',
+    _hover: {
+      backgroundColor: 'slate.900'
+    }
   })
 
   return <h1 className={styles}>Hello world</h1>
--- react-panda/vite.config.ts
+++ react-panda/vite.config.ts
@@ -1,36 +1,32 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 
-// import dsv from '@rollup/plugin-dsv'
-// import FullReload from 'vite-plugin-full-reload'
-// plugins: [react(), FullReload(['src/app.tsx']), dsv()]
-
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
-    plugins: [react()], 
+    plugins: [react()],
 
     server: {
       port: parseInt(process.env.VITE_PORT!),
       host: true,
       https: {
         key: `${process.env.HOME}/.config/ssl/local/local.key`,
-        cert: `${process.env.HOME}/.config/ssl/local/local.crt`
-      }
+        cert: `${process.env.HOME}/.config/ssl/local/local.crt`,
+      },
     },
 
     resolve: {
       alias: [
         {
           find: 'src',
-          replacement: `${__dirname}/src`
+          replacement: `${__dirname}/src`,
         },
         {
           find: 'styled-system',
-          replacement: `${__dirname}/styled-system`
-        }
-      ]
-    }
+          replacement: `${__dirname}/styled-system`,
+        },
+      ],
+    },
   })
 }
